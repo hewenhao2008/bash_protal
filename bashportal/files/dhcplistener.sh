0a69f7b46689f2bd94b4d36e77da1f397c246d75
czhongm <zm.chu@tgrass.com>
2015-01-27 16:35:00 +0800

    增加dhcp分配ip地址时的触发脚本，功能待完成

---
+++ bashportal/files/dhcplistener.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Bash编写的用于处理dnsmasq分配ip地址时触发认证处理
+# 添加在dnsmasq
+# 
+
+# Date    : 2015-01-27
+# Version : 1.0
+# Author  : czhongm <czhongm@gmail.com>
+
+IPTABLES=/usr/sbin/iptables
+
+exit 0
+        
