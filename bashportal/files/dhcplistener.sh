d277573c53176fe82f3798507ae063cabace4608
czhongm <czhongm@gmail.com>
2015-03-04 10:38:44 +0800

    1、简化dnsmasq触发脚本，根据dnsmasq的interface判断触发对应的脚本 2、修改启动脚本使得自动生成dnsmasq触发脚本

--- bashportal/files/dhcplistener.sh
+++ bashportal/files/dhcplistener.sh
@@ -8,35 +8,9 @@
 # Version : 1.0
 # Author  : czhongm <czhongm@gmail.com>
 
-IPTABLES=/usr/sbin/iptables
-
-. $IPKG_INSTROOT/lib/functions.sh
-
-action=$1
-mac=$2
-ip=$3
-hostname=$4
-
-auth_client(){
-	local cfg="$1"
-	local interface extinterface trustfile validfile ip validlog knownlog mac portalport
-	local portal_url auth_url welcome_url
-	local gw_address gw_mac gw_id
-	config_get interface "$cfg" interface
-	if [ "$DNSMASQ_INTERFACE" = "$interface" ]; then
-		. /tmp/portal/$cfg/env.sh
-		. /tgrass/portal/libportal.sh
-		if [ $action = "add" -o $action = "old" ]; then
-			proc_iptables $mac $ip $hostname
-		elif [ $action = "del" ]; then
-			clear_valid_flag $mac
-			clear_known_flag $mac
-		fi
-	fi
-}
-
-config_load bashportal
-config_foreach auth_client portal
+if [ -f /tmp/portal/${DNSMASQ_INTERFACE}.sh ]; then
+	/tmp/portal/${DNSMASQ_INTERFACE}.sh $1 $2 $3 $4
+fi
 
 exit 0
         
